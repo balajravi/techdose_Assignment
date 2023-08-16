3d9e8645f13eb695560578d601ce7cc73c6b49a1
balaji-ravi2104 <108446931+balaji-ravi2104@users.noreply.github.com>
2023-08-16 18:40:41 +0530

    Update Merge 2 sorted arrays without using extra space.java

--- Array/Merge 2 sorted arrays without using extra space.java
+++ Array/Merge 2 sorted arrays without using extra space.java
@@ -1,64 +1,72 @@
+Time Complexity: O((n+m)*log(n+m)), where n and m are the sizes of the given arrays.
+
+Space Complexity: O(1) as we are not using any extra space.
+
+
 import java.util.*;
-public class Main {
 
-  public static void main(String[] args) {
-    int arr1[] = {1,4,7,8,10};
-	int arr2[] = {2,3,9};
-    System.out.println("Before merge:");
-    for (int i = 0; i < arr1.length; i++) {
-      System.out.print(arr1[i] + " ");
-    }
-    System.out.println();
-    for (int i = 0; i < arr2.length; i++) {
-      System.out.print(arr2[i] + " ");
-    }
-    System.out.println();
-    merge(arr1, arr2, arr1.length, arr2.length);
-    System.out.println("After merge:");
-    for (int i = 0; i < arr1.length; i++) {
-      System.out.print(arr1[i] + " ");
-    }
-    System.out.println();
-    for (int i = 0; i < arr2.length; i++) {
-      System.out.print(arr2[i] + " ");
+public class tUf {
+
+    public static void swapIfGreater(long[] arr1, long[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            long temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
+        }
     }
 
-  }
-  static void merge(int[] arr1, int arr2[], int n, int m) {
-    // code here
-    int i, k;
-    for (i = 0; i < n; i++) {
-      // take first element from arr1 
-      // compare it with first element of second array
-      // if condition match, then swap
-      if (arr1[i] > arr2[0]) {
-        int temp = arr1[i];
-        arr1[i] = arr2[0];
-        arr2[0] = temp;
-      }
+    public static void merge(long[] arr1, long[] arr2, int n, int m) {
 
-      // then sort the second array
-      // put the element in its correct position
-      // so that next cycle can swap elements correctly
-      int first = arr2[0];
-      // insertion sort is used here
-      for (k = 1; k < m && arr2[k] < first; k++) {
-        arr2[k - 1] = arr2[k];
-      }
-      arr2[k - 1] = first;
+        // len of the imaginary single array:
+        int len = n + m;
+
+        // Initial gap:
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            // Place 2 pointers:
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                // case 1: left in arr1[]
+                //and right in arr2[]:
+                if (left < n && right >= n) {
+                    swapIfGreater(arr1, arr2, left, right - n);
+                }
+                // case 2: both pointers in arr2[]:
+                else if (left >= n) {
+                    swapIfGreater(arr2, arr2, left - n, right - n);
+                }
+                // case 3: both pointers in arr1[]:
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++; right++;
+            }
+            // break if iteration gap=1 is completed:
+            if (gap == 1) break;
+
+            // Otherwise, calculate new gap:
+            gap = (gap / 2) + (gap % 2);
+        }
     }
-  }
+
+    public static void main(String[] args) {
+        long[] arr1 = {1, 4, 8, 10};
+        long[] arr2 = {2, 3, 9};
+        int n = 4, m = 3;
+        merge(arr1, arr2, n, m);
+        System.out.println("The merged arrays are:");
+        System.out.print("arr1[] = ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.print("\narr2[] = ");
+        for (int i = 0; i < m; i++) {
+            System.out.print(arr2[i] + " ");
+        }
+        System.out.println();
+    }
+
 }
 
-/*
-Before merge:
-1 4 7 8 10
-2 3 9
-After merge:
-1 2 3 4 7
-8 9 10
-
-Time complexity: O(n*m)
-
-Space Complexity: O(1) 
-/
