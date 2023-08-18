3f4cce65b947ebf865a7cc17cf5aa983f4e48dc4
balajravi <128625737+balajravi@users.noreply.github.com>
2023-08-18 20:30:35 +0530

    Next Greater Element I.java

--- Stack & Queue/Next Greater Element I.java
+++ Stack & Queue/Next Greater Element I.java
@@ -1,3 +1,22 @@
+// Optimal Way
+class Solution {
+        public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+            // System.out.println(map);
+        }   
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+
+        return findNums;
+    }
+}
+
+
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         Map<Integer,Integer> map =new HashMap<>();
