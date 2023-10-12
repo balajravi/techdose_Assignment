4070643cffa1c1f01aaf74e2605df694e94bc672
balajravi <128625737+balajravi@users.noreply.github.com>
2023-10-12 09:53:27 +0530

    Update Search in Rotated Sorted Array.java

--- Array/Search in Rotated Sorted Array.java
+++ Array/Search in Rotated Sorted Array.java
@@ -1,27 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int left=0;
-        int right=n-1;
-        int mid;
-        while(left<=right){
-            mid=(left+right)/2;
-            if(nums[mid]==target)
-                return mid;
-            else if(nums[mid]>=nums[left]){
-                if(target<=nums[mid] && target>=nums[left])
-                    right=mid-1;
-                else
-                    left=mid+1;
+      int start=0;
+        int end=nums.length-1;
+        
+        while(start<=end){
+        int mid= start+(end-start) /2;
+        if(nums[mid]==target) 
+            return mid;
+        if(nums[start]<=nums[mid]){
+            if(target>=nums[start] && target<=nums[mid]){
+                end=mid-1;
             }else{
-                if(target>=nums[mid] && target<=nums[right])
-                    left=mid+1;
-                else
-                    right=mid-1;
+                start=mid+1;
             }
+        }
+        else{
+            if(target>=nums[mid] && target<=nums[end]){  
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
         }
         return -1;
     }
+    
 }
 /*
 There is an integer array nums sorted in ascending order (with distinct values).
